9d8424d2435532424281a5f2353242853a74d2ea
Kirill_divine <kirill.sokolov96@gmail.com>
2023-03-04 14:04:17 +0200

    added binary search algorithm implementation + tests

---
+++ Algorithms.Tests/BinarySearchTests.cs
@@ -0,0 +1,63 @@
+﻿using Xunit;
+
+namespace Algorithms.Tests
+{
+    public class BinarySearchTests
+    {
+        [Fact]
+        public void TestBinarySearch_FoundValue()
+        {
+            // Arrange
+            int[] arr = { 1, 3, 5, 7, 9 };
+            int x = 5;
+
+            // Act
+            int result = BinarySearch.Search(arr, x);
+
+            // Assert
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void TestBinarySearch_FoundValueAtEnd()
+        {
+            // Arrange
+            int[] arr = { 1, 3, 5, 7, 9, 11 };
+            int x = 11;
+
+            // Act
+            int result = BinarySearch.Search(arr, x);
+
+            // Assert
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public void TestBinarySearch_ValueNotFound()
+        {
+            // Arrange
+            int[] arr = { 1, 3, 5, 7, 9 };
+            int x = 4;
+
+            // Act
+            int result = BinarySearch.Search(arr, x);
+
+            // Assert
+            Assert.Equal(-1, result);
+        }
+
+        [Fact]
+        public void TestBinarySearch_EmptyArray()
+        {
+            // Arrange
+            int[] arr = { };
+            int x = 5;
+
+            // Act
+            int result = BinarySearch.Search(arr, x);
+
+            // Assert
+            Assert.Equal(-1, result);
+        }
+    }
+}
---
+++ Algorithms/BinarySearch.cs
@@ -0,0 +1,31 @@
+﻿namespace Algorithms
+{
+    public static class BinarySearch
+    {
+        public static int Search(int[] arr, int x)
+        {
+            int left = 0;
+            int right = arr.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+
+                if (arr[mid] == x)
+                {
+                    return mid;
+                }
+                else if (arr[mid] < x)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
