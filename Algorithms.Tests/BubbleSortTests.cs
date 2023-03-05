3b6a9013453fbe4fe854a70a885b4853a5c28585
Kirill_divine <kirill.sokolov96@gmail.com>
2023-03-05 13:35:56 +0200

    added bubble sort algorithm implementation + tests

---
+++ Algorithms.Tests/BubbleSortTests.cs
@@ -0,0 +1,46 @@
+﻿using Xunit;
+
+namespace Algorithms.Tests
+{
+    public class BubbleSortTests
+    {
+        [Fact]
+        public void SortsIntArrayInAscendingOrder()
+        {
+            // Arrange
+            int[] arr = { 5, 2, 8, 1, 4 };
+
+            // Act
+            BubbleSort.Sort(arr);
+
+            // Assert
+            Assert.Equal(new int[] { 1, 2, 4, 5, 8 }, arr);
+        }
+
+        [Fact]
+        public void SortsEmptyIntArray()
+        {
+            // Arrange
+            int[] arr = { };
+
+            // Act
+            BubbleSort.Sort(arr);
+
+            // Assert
+            Assert.Equal(new int[] { }, arr);
+        }
+
+        [Fact]
+        public void SortsSingleIntArray()
+        {
+            // Arrange
+            int[] arr = { 5 };
+
+            // Act
+            BubbleSort.Sort(arr);
+
+            // Assert
+            Assert.Equal(new int[] { 5 }, arr);
+        }
+    }
+}
---
+++ Algorithms/BubbleSort.cs
@@ -0,0 +1,23 @@
+﻿namespace Algorithms
+{
+    public static class BubbleSort
+    {
+        public static void Sort(int[] arr)
+        {
+            int length = arr.Length;
+            for (int i = 0; i < length - 1; i++)
+            {
+                for (int j = 0; j < length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+    }
+}
